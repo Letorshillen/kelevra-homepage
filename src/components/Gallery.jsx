021937fa79cb590ef96a0691941a7b791210fa47
Letorshillen <stom.brehm@t-online.de>
2021-06-17 17:42:32 +0200

    set up schema

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client";
 
 const Gallery = () => {
-  return <h1>gallery</h1>;
+  const [galleryData, setGallery] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_tpye=="drawing"]{
+      name,
+      drawing_image{
+        asset->{
+          _id,
+          url
+        },
+        alt
+      }
+    }`
+      )
+      .then((data) => {
+        console.log(data);
+        setGallery(data);
+      })
+
+      .catch(console.error);
+  }, []);
+
+  return <main></main>;
 };
 
 export default Gallery;
