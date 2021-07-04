ab137b024515fe4a2ea6fc8b2d1cde28e157c93e
Letorshillen <stom.brehm@t-online.de>
2021-07-04 16:31:02 +0200

    added pokemon card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,22 +5,11 @@ import Scene from "./threeScenes/SceneAbout";
 const About = () => {
   return (
     <main>
-      <h1 className="font-h mt-28 text-8xl text-center">
+      <h2 className="font-h mt-28 text-8xl text-center">
         meet the <br /> artist
-      </h1>
-      <div className="flex m-4">
-        <img src={aboutImage} alt="Sophie Kelevra" className="w-1/3" />
-        <p className="w-1/3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, facilis
-          impedit provident sint illo in sed dolorum similique earum ad fugit,
-          nostrum adipisci praesentium reiciendis, a debitis consequuntur
-          recusandae architecto dolores quibusdam porro aut temporibus. Illum
-          quidem iusto eum odio inventore error cumque voluptatibus, eius illo
-          animi ullam incidunt quo.
-        </p>
-        <div>
-          <Scene />
-        </div>
+      </h2>
+      <div className="w-full h-m">
+        <Scene />
       </div>
     </main>
   );
--- src/components/threeScenes/SceneAbout.jsx
+++ src/components/threeScenes/SceneAbout.jsx
@@ -1,23 +1,62 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+import img1 from "../../static/card1.jpg";
+import backImg from "../../static/back.jpg";
+import { OrbitControls, useTexture } from "@react-three/drei";
 
 const Card = () => {
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    mesh.current.position.y = Math.sin(a) * 0.2;
+  });
+
+  const mesh = useRef();
+  const backside = useTexture(backImg);
+  const frontside1 = useTexture(img1);
+
   return (
-    <mesh onPointerOver={(e) => console.log("over")}>
-      <planeGeometry />
-      <meshPhongMaterial color="royalblue" />
+    <mesh ref={mesh} scale={0.6} position={[0, 0, 0]}>
+      <boxGeometry attach="geometry" args={[4, 6, 0.02]} />
+      <meshPhongMaterial
+        shininess={80}
+        attachArray="material"
+        color="#080e29"
+      />
+      <meshPhongMaterial
+        shininess={80}
+        attachArray="material"
+        color="#080e29"
+      />
+      <meshPhongMaterial
+        shininess={80}
+        attachArray="material"
+        color="#080e29"
+      />
+      <meshPhongMaterial
+        shininess={80}
+        attachArray="material"
+        color="#080e29"
+      />
+      <meshPhongMaterial shininess={80} attachArray="material" map={backside} />
+      <meshBasicMaterial
+        shininess={80}
+        attachArray="material"
+        map={frontside1}
+      />
     </mesh>
   );
 };
 
 const Scene = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
+    <Canvas linear={true}>
+      <ambientLight intensity={0.8} />
       <directionalLight intensity={0.5} position={[2, 1, 3]} color="white" />
       <Suspense fallback={null}>
         <Card />
       </Suspense>
+      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
     </Canvas>
   );
 };
